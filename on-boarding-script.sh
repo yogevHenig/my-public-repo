5e418a505b135f1c326233737f36a21360091471
Yogev Henig <yogevhenig@gmail.com>
2023-02-23 09:19:54 +0200

    Update on-boarding-script.sh

--- on-boarding-script.sh
+++ on-boarding-script.sh
@@ -1,10 +1,10 @@
 
-
+client_id = $(az ad sp show --id $1 --query id -o tsv)
 
 az group create --name awp-scan-rg --location eastus
 
-az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $(az ad sp show --id $1 --query id -o tsv) --role "Disk Snapshot Contributor" --resource-group awp-scan-rg
+az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $client_id --role "Disk Snapshot Contributor" --resource-group awp-scan-rg
 
-az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $(az ad sp show --id $1  --query id -o tsv) --role "Network Contributor" --resource-group awp-scan-rg
+az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $client_id --role "Network Contributor" --resource-group awp-scan-rg
 
-az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $(az ad sp show --id $1  --query id -o tsv) --role "Virtual Machine Contributor" --resource-group awp-scan-rg
+az role assignment create --assignee-principal-type ServicePrincipal  --assignee-object-id $client_id --role "Virtual Machine Contributor" --resource-group awp-scan-rg
